8be06fee8bc828550540ea70b55d2d288659eb6f
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-03 08:06:44 +0600

    news card done

--- js/app.js
+++ js/app.js
@@ -28,6 +28,34 @@ const loadMenu = async (id) => {
 
 const displayMenu = (data) => {
     document.getElementById('total-item').innerText = data.length;
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = ``;
+    data.forEach(element => {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerHTML =
+            `
+        <div class="d-flex justify-content-center">
+                    <div class="card mb-3" style="max-width: 80%;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${element.title}</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural
+                                        lead-in to
+                                        additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        cardContainer.appendChild(cardDiv);
+
+    });
     console.log(data);
 }
 
